abea545dc8772ff2d708b5a1d4744d8f7e748d68
unknown <saurabhkatwal@gmail.com>
2021-11-07 02:28:27 +0530

    third commit

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({
+    extended: true
+}));
 app.get('/', function (req, res) {
     var jday = new Date();
     var currDay = jday.getDay();
@@ -16,7 +19,11 @@ app.get('/', function (req, res) {
     res.render('list', {
         kindOfDay: day
     });
-})
+});
+app.post('/', function (req, res) {
+    var tasks = req.body.tname;
+    console.log('task is ' + tname);
+});
 app.listen(3000, function () {
     console.log('server is running at port 3000');
-})
+});
