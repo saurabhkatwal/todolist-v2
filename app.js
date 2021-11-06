f5d6055581c8bce54ed6e7aaf00b4425f3d72902
unknown <saurabhkatwal@gmail.com>
2021-11-07 02:01:54 +0530

    made some small modifications

--- app.js
+++ app.js
@@ -1,38 +1,22 @@
-const express=require('express');
-const app=express();
-app.set('view engine','ejs');
-app.get('/',function(req,res){
-var jday=new Date();
-var currDay=jday.getDay();
-var day='';
-switch(currDay){
-    case 0:
-        day='sunday';
-        break;
-    case 1:
-        day='monday';
-        break;
-    case 2:
-        day='tuesday';
-        break;
-    case 3:
-        day='wednesday';
-        break;
-    case 4:
-        day='thursday';
-        break;
-    case 5:
-        day='friday';
-        break;
-    case 6:
-        day='saturday';
-        break;
-    default:
-        console.log('error: the current day is '+currDay);
-        break;
-}
-res.render('list',{kindOfDay:day});
+const express = require('express');
+const app = express();
+app.set('view engine', 'ejs');
+app.get('/', function (req, res) {
+    var jday = new Date();
+    var currDay = jday.getDay();
+    var day = '';
+    var options = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    };
+    var today = new Date();
+    day = today.toLocaleDateString(undefined, options);
+    res.render('list', {
+        kindOfDay: day
+    });
 })
-app.listen(3000,function(){
+app.listen(3000, function () {
     console.log('server is running at port 3000');
 })
